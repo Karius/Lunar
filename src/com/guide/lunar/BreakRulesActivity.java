f9d32e80c0acc878c059bc13646926b7c01815ab
Karius <kittarius@gmail.com>
2012-05-26 14:08:46 +0800

    完成违章信息页面的 Tab 显示与控制;

--- src/com/guide/lunar/BreakRulesActivity.java
+++ src/com/guide/lunar/BreakRulesActivity.java
@@ -4,10 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Button;
 import android.widget.TabHost;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.RadioButton;
 
 public class BreakRulesActivity extends Activity {
@@ -19,50 +18,55 @@ public class BreakRulesActivity extends Activity {
 		
 		initComponents ();
 		setListeners ();
+		
+		setRemoteCount (1);
 	}
 	
 	private Button button_back;
 	private TabHost tabHost;
-	private RadioGroup tabGroup;
 	private RadioButton tabBtn1;
 	private RadioButton tabBtn2;
+	private TextView tvLocalCount;
+	private TextView tvRemoteCount;
 	
     private void initComponents ()
     {
     	button_back = (Button) findViewById(R.id.btnBack);
-    	tabGroup = (RadioGroup) findViewById (R.id.tab_group);
-    	tabBtn1 = (RadioButton) findViewById (R.id.tab1);
-    	tabBtn2 = (RadioButton) findViewById (R.id.tab2);
+    	
+    	tvLocalCount = (TextView) findViewById (R.id.tvLocalCount);
+    	tvRemoteCount = (TextView) findViewById (R.id.tvRemoteCount);
+   	
+    	tabBtn1 = (RadioButton) findViewById (R.id.tabBtnLocal);
+    	tabBtn2 = (RadioButton) findViewById (R.id.tabBtnRemote);
     	
     	tabHost = (TabHost) findViewById (android.R.id.tabhost);
     	tabHost.setup ();
     	
     	tabHost.addTab (tabHost.newTabSpec( "tag1" ).setContent(R.id.tab1).setIndicator (getResources ().getString(R.string.tabLocalInfo)));
     	tabHost.addTab (tabHost.newTabSpec( "tag2" ).setContent(R.id.tab2).setIndicator (getResources ().getString(R.string.tabRemoteInfo)));
-    	
-    	tabGroup.setOnCheckedChangeListener(new OnTabChangeListener());
-    	
-    	
-    	//TabHost.TabSpec spec1 = tabHost.newTabSpec( "tag1" );
-    	//spec1.setContent(R.id.tab1);
-    	//spec1.setIndicator ("本地");
-    	
-    	//tabHost.addTab(spec1);
-    	
-        /*field_height = (EditText) findViewById(R.id.height);
-        field_weight = (EditText) findViewById(R.id.weight);
-        view_result = (TextView) findViewById(R.id.result);
-        view_suggest = (TextView) findViewById(R.id.suggest);*/
+
+    }
+    
+    private void setLocalCount (int count) {
+    	tvLocalCount.setVisibility(count);
+    	tvLocalCount.setText(Integer.toString(count));
+    }
+    
+    private void setRemoteCount (int count) {
+    	tvRemoteCount.setVisibility(count);
+    	tvRemoteCount.setText(Integer.toString(count));
     }
     
     private class OnTabClickListener implements View.OnClickListener {
       public void onClick(View v)
       {
     	  if (v == tabBtn1) {
+    		  tabHost.setCurrentTab(0);
     		  tabBtn1.setChecked(true);
     		  tabBtn2.setChecked(false);
     	  }
     	  else if (v == tabBtn2) {
+    		  tabHost.setCurrentTab(1);
     		  tabBtn1.setChecked(false);
     		  tabBtn2.setChecked(true);
     	  }
@@ -82,23 +86,4 @@ public class BreakRulesActivity extends Activity {
     	tabBtn2.setOnClickListener(new OnTabClickListener ());
      }
     
-    
-    private class OnTabChangeListener implements OnCheckedChangeListener {
-    	//@Override
-        public void onCheckedChanged (RadioGroup group, int id) {
-        	//Button btn = null;
-        	//btn.setClickable(false);
-            //尤其需要注意这里，setCurrentTabByTag方法是纽带
-            switch (id) {
-            case R.id.tab1:
-                tabHost.setCurrentTabByTag("tag1");
-                break;
-            case R.id.tab2:
-            	tabHost.setCurrentTabByTag("tag2");
-                break;
- 
-            }
- 
-        }
-    }
-}
+ }
