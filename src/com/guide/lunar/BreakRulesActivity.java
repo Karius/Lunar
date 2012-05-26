480885f88a9b9ec807976b8c761687fc0fdc3659
Karius <kittarius@gmail.com>
2012-05-26 16:26:12 +0800

    基本固定了底部 Tab 的实现方法.

--- src/com/guide/lunar/BreakRulesActivity.java
+++ src/com/guide/lunar/BreakRulesActivity.java
@@ -10,6 +10,11 @@ import android.widget.TabHost;
 import android.widget.RadioButton;
 
 public class BreakRulesActivity extends Activity {
+	
+	public static final int LOCAL  = 0;
+	public static final int REMOTE = 1;
+
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,13 +24,13 @@ public class BreakRulesActivity extends Activity {
 		initComponents ();
 		setListeners ();
 		
-		setRemoteCount (1);
+		setViolationCount (LOCAL, 0);
+		setViolationCount (REMOTE, 1);
 	}
 	
 	private Button button_back;
 	private TabHost tabHost;
-	private RadioButton tabBtn1;
-	private RadioButton tabBtn2;
+	private RadioButton[] tabBtnArray;
 	private TextView tvLocalCount;
 	private TextView tvRemoteCount;
 	
@@ -36,39 +41,35 @@ public class BreakRulesActivity extends Activity {
     	tvLocalCount = (TextView) findViewById (R.id.tvLocalCount);
     	tvRemoteCount = (TextView) findViewById (R.id.tvRemoteCount);
    	
-    	tabBtn1 = (RadioButton) findViewById (R.id.tabBtnLocal);
-    	tabBtn2 = (RadioButton) findViewById (R.id.tabBtnRemote);
+    	tabBtnArray = new RadioButton[2];
+    	tabBtnArray[0] = (RadioButton) findViewById (R.id.tabBtnLocal);
+    	tabBtnArray[1] = (RadioButton) findViewById (R.id.tabBtnRemote);
     	
     	tabHost = (TabHost) findViewById (android.R.id.tabhost);
-    	tabHost.setup ();
-    	
+    	tabHost.setup ();    	
     	tabHost.addTab (tabHost.newTabSpec( "tag1" ).setContent(R.id.tab1).setIndicator (getResources ().getString(R.string.tabLocalInfo)));
     	tabHost.addTab (tabHost.newTabSpec( "tag2" ).setContent(R.id.tab2).setIndicator (getResources ().getString(R.string.tabRemoteInfo)));
 
     }
     
-    private void setLocalCount (int count) {
-    	tvLocalCount.setVisibility(count);
-    	tvLocalCount.setText(Integer.toString(count));
-    }
-    
-    private void setRemoteCount (int count) {
-    	tvRemoteCount.setVisibility(count);
-    	tvRemoteCount.setText(Integer.toString(count));
+    private void setViolationCount (int btnType, int count) {
+    	TextView tv = (btnType == LOCAL) ? tvLocalCount : tvRemoteCount;
+    	tv.setVisibility((count > 0) ? View.VISIBLE : View.INVISIBLE);
+    	tv.setText(Integer.toString(count));
     }
     
     private class OnTabClickListener implements View.OnClickListener {
       public void onClick(View v)
       {
-    	  if (v == tabBtn1) {
-    		  tabHost.setCurrentTab(0);
-    		  tabBtn1.setChecked(true);
-    		  tabBtn2.setChecked(false);
-    	  }
-    	  else if (v == tabBtn2) {
-    		  tabHost.setCurrentTab(1);
-    		  tabBtn1.setChecked(false);
-    		  tabBtn2.setChecked(true);
+    	  int index = 0;
+    	  for (RadioButton tabBtn:tabBtnArray) {
+    		  if (v == tabBtn) {
+    			  tabBtn.setChecked(true);
+    			  tabHost.setCurrentTab(index);
+    		  } else {
+    			  tabBtn.setChecked(false);
+    		  }
+    		  index++;
     	  }
       }
 	}
@@ -82,8 +83,9 @@ public class BreakRulesActivity extends Activity {
 	          }
 	    	});
     	
-    	tabBtn1.setOnClickListener(new OnTabClickListener ());
-    	tabBtn2.setOnClickListener(new OnTabClickListener ());
+    	for (RadioButton tabBtn:tabBtnArray) {
+    		tabBtn.setOnClickListener(new OnTabClickListener ());
+    	}
      }
     
  }
