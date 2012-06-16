14fd8565d831a7fb26ff88008c713860042c8894
Karius <kittarius@gmail.com>
2012-06-16 16:47:24 +0800

    改进TabBand.setCurrentTab(int pos)实现方法

--- src/com/guide/lunar/TabBand.java
+++ src/com/guide/lunar/TabBand.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class TabBand {
 	
+	// 每个 Tab携带的数据
 	private class TabButton {
 		private int id;
 		private RadioButton mTabButton;
@@ -18,9 +19,17 @@ public class TabBand {
 		private Object userData = null;
 	}
 	
+	// TabHost
 	private TabHost mMainTabHost;
+	// 所有的Tab Button集合
 	private List<TabButton> mTabButtons;
+	
+	// 当前焦点的 TabButton
+	private TabButton mCurrTabButton = null;
+	
+	// badge选中时的背景资源ID
 	int mFocusBg;
+	// badge失焦时的背景资源ID
 	int mDefocusBg;
 	
 	public TabBand (TabHost mainTabHost, int focusBg, int defocusBg) {
@@ -50,41 +59,46 @@ public class TabBand {
 		return this;
 	}
 
+	// 将指定索引的RadioButton设置为当前选中的
 	public TabBand setCurrentTab (int pos) {
-    	for (int index=0;index<mTabButtons.size();index++) {
-    		TabButton tb = mTabButtons.get(index);
-    		if (pos == index) {
-    			mMainTabHost.setCurrentTab(index);
-    			tb.mTabButton.setChecked(true);
-    			tb.mBadgeNumber.setBackgroundResource(mFocusBg);
-    		} else {
-    			tb.mTabButton.setChecked(false);
-    			tb.mBadgeNumber.setBackgroundResource(mDefocusBg);
-    		}
-    	}
-    	return this;
+		TabButton prevTb = mCurrTabButton;
+		TabButton tb = mTabButtons.get(pos);
+		if (null != tb) {
+			mCurrTabButton = tb;
+			mMainTabHost.setCurrentTab(pos);
+			tb.mTabButton.setChecked(true);
+			tb.mBadgeNumber.setBackgroundResource(mFocusBg);
+			if (prevTb != null) {
+				prevTb.mTabButton.setChecked(false);
+    			prevTb.mBadgeNumber.setBackgroundResource(mDefocusBg);
+			}
+		}
+		return this;
     }
-    
+
+	// 将指定View对象的RadioButton设置为当前选中的    
     public TabBand setCurrentTab (View v) {
     	for (int i=0;i<mTabButtons.size();i++) {
     		if (v == mTabButtons.get(i).mTabButton) {
     			return setCurrentTab (i);
     		}
-    	}
-    	
+    	}    	
     	return this;
     }
 
+    // 设置右上角圆图标中的数字值
     public void setBadgeNumber (int btnId, int number) {
 		TextView tv = findById (btnId).mBadgeNumber;
 		tv.setVisibility((number > 0) ? View.VISIBLE : View.INVISIBLE);
 		tv.setText(Integer.toString(number));
    	}
     
+    // 根据ID查找用户数据
     public Object getUserData (int btnId) {
     	return findById (btnId).userData;
     }
 
+    // 根据ID查找RadioButton
     public TabButton findById (int btnId) {
     	for (TabButton tb:mTabButtons) {
     		if (btnId == tb.id) {
@@ -94,12 +108,14 @@ public class TabBand {
     	return null;
     }
 
+    // 将点击事件设置到每一个RadioButton上
     public TabBand setListener () {
     	for (TabButton tb:mTabButtons) {
     		tb.mTabButton.setOnClickListener(new OnTabClickListener ());
     	}
     	return this;
-    }    
+    }
+    // RadioButton 按钮点击触摸16:29
     private class OnTabClickListener implements View.OnClickListener {
         public void onClick(View v)
         {
